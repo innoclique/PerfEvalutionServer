5a16969be54ca95aa2f0b9405becfeef146559b0
Venu Rai <venrai2000@gmail.com>
2020-12-22 22:50:59 +0530

    => added direct reposrts

--- Controller/EmployeeManagerController.js
+++ Controller/EmployeeManagerController.js
@@ -5,3 +5,9 @@ exports.Dashboard = async (req,res,next)=>{
         .then(Response => Response ? res.status(200).json(Response) : res.status(404).json(""))
         .catch(err => next(err => { next(err) }));
 }
+
+exports.DirectReposrtsCtrl = async (req,res,next)=>{
+    await EmployeeManagerService.DirectReports(req.body)
+        .then(Response => Response ? res.status(200).json(Response) : res.status(404).json(""))
+        .catch(err => next(err => { next(err) }));
+}
--- Routes/EmployeeManagerRouter.js
+++ Routes/EmployeeManagerRouter.js
@@ -3,5 +3,6 @@ const router = Express.Router();
 const EmployeeManagerController = require('../Controller/EmployeeManagerController');
 
 router.post("/dashboard", EmployeeManagerController.Dashboard);
+router.post("/direct/reports", EmployeeManagerController.DirectReposrtsCtrl);
 
 module.exports = router;

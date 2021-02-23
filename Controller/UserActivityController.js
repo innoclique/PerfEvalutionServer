7c0dbf1e883a73bbc6d6eae1ee7354bb5f8deaa3
GSANGLI <govindmailid@gmail.com>
2021-02-23 09:30:29 +0530

    user activity logger changes

---
+++ Controller/UserActivityController.js
@@ -0,0 +1,15 @@
+const { LogUserActivity } = require('../DataServices/ActivityService');
+
+const userActivityCtrl = async (req, res, next) => {
+    console.log('inside userActivityCtrl ', req.body);
+    await LogUserActivity(req.body)
+        .then(Response => res.status(200).json(Response))
+        .catch(error => {
+            logger.error(error)
+            next(error)
+        });
+}
+
+module.exports = {
+    UserActivityCtrl: userActivityCtrl
+}

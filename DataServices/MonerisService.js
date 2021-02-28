256b6115191aec1edc6e18c6a02613fbbadb36da
Venu Rai <venrai2000@gmail.com>
2021-02-28 21:09:43 +0530

    => moneris changes

--- DataServices/MonerisService.js
+++ DataServices/MonerisService.js
@@ -1,10 +1,12 @@
 const got = require("got");
 const { v4: uuidv4 } = require('uuid');
 const {MonerisConfig} = require("../utils/MonerisUtils")
-
-const postUrl = "https://gatewayt.moneris.com/chkt/request/request.php";
+var env = process.env.NODE_ENV || "dev";
+var config = require(`../Config/${env}.config`);
 
 const getTicket = async (options) => {
+    let {monerisSettings} = config;
+    
     console.log(options);
     let requestObj = MonerisConfig();
     let {payableAmount,transactionId} = options;
@@ -14,7 +16,7 @@ const getTicket = async (options) => {
         console.log('inside getTicket');
         console.log("===MONARIES-REQUEST===");
         console.log(requestObj);
-        const {body} = await got.post(postUrl, {
+        const {body} = await got.post(monerisSettings.GATEWAY_URL, {
             json: requestObj,
             responseType: 'json'
         });

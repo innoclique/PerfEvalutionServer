2acffe8b662c17fc875e8af3b98a68afa796c3d0
Venu Rai <venrai2000@gmail.com>
2021-03-08 14:49:51 +0530

    => Peer director reporties request changes

--- Controller/EmployeeManagerController.js
+++ Controller/EmployeeManagerController.js
@@ -23,3 +23,8 @@ exports.FindPeerDirectReportRequestCtrl = async (req,res,next)=>{
     let requestList =  await EmployeeManagerService.FindPeerDirectReportRequest(req.body);
     res.status(200).json(requestList);
 }
+exports.FindPeerDirectReportRequestByEmployeeCtrl = async (req,res,next)=>{
+    console.log("Inside:FindPeerDirectReportRequestByEmployeeCtrl");
+    let requestList =  await EmployeeManagerService.FindPeerDirectReportRequestByEmployee(req.body);
+    res.status(200).json(requestList);
+}
--- SchemaModels/PeerDirectReportsRequestSchema.js
+++ SchemaModels/PeerDirectReportsRequestSchema.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const PeerDirectReportsRequestsSchema = new mongoose.Schema({
     EmployeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'User',default:null },    
     IsDraft:{type:Boolean, default:false},
-    DisplayTemplate:{type:String},
-    Competencies:[{_id:{type:String},Name:{type:String}}],
+    Peer:[
+        {
+            EmployeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'User',default:null },  
+            displayTemplate:{type:String},
+            Competencies:[{_id:{type:String},Name:{type:String}}],
+        }
+    ],
+    DirectReportees:[
+        {
+            EmployeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'User',default:null },  
+            displayTemplate:{type:String},
+            Competencies:[{_id:{type:String},Name:{type:String}}],
+        }
+    ],
     CreatedBy:{ type: mongoose.Schema.Types.ObjectId, ref: 'User',default:null },
     CreatedOn:{type:Date,default: Date() },
     Type:{type:String},
